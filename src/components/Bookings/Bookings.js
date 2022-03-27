2ccb81677f4af1eebd5953e324a526de6be03a56
Sakif <sakifworkspace@gmail.com>
2022-03-28 00:07:23 +0600

    to show users based data,here is used userContext and in useEffect,loggedInUser is for user based validation

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Bookings = () => {
     const [bookings, setBookings] = useState ([]);
+    const [loggedInUser,setLoggedInUser] = useContext(UserContext);
 
     useEffect(()=>{
-        fetch('http://localhost:5000/bookings')
+        fetch('http://localhost:5000/bookings?email='+loggedInUser.email)
         .then(res=> res.json())
         .then(data=>setBookings(data))
     },[])
