d396d53787612ed17aa693e21df523a57b97c328
Sakif <sakifworkspace@gmail.com>
2022-03-27 13:47:31 +0600

    add picker,date-time picker,manage check in check out date

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,68 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import '@date-io/date-fns';
 import { Link, useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import './Book.css';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { Button } from '@material-ui/core';
 
 const Book = () => {
     const [loggedInUser,setLoggedInUser] = useContext(UserContext)
     const {bedType} = useParams();
+
+    const [selectedDate, setSelectedDate] = useState({
+      checkIn: new Date(),
+      checkOut: new Date()
+    });
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
     return (
         <div style={{textAlign: 'center'}}>
             <h1>Hello {loggedInUser.name}, Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+
+            
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-between">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate.checkIn}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          value={selectedDate.checkOut}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+      <Button variant="contained" color="primary">
+  Book Now
+</Button>
+    </MuiPickersUtilsProvider>
+ 
+
         </div>
     );
 };
